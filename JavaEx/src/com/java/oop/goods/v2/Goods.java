c967af6a22ec87db6a96c3ce640fd685130a9494
Parksanghyun <skfnxh718@naver.com>
2020-08-12 12:45:05 +0900

    getter와 setter

--- JavaEx/src/com/java/oop/goods/v2/Goods.java
+++ JavaEx/src/com/java/oop/goods/v2/Goods.java
@@ -8,13 +8,34 @@ public class Goods {
 	// 접근자가 없으면 default
 	// 클래스 내부 or 동일 패지키 내에서 접근 가능
 	
+	/*
 	String name;
 	int price;
-	
+	*/
 	
 	// public >> protected >> default >> private
-	/*
-	private String naem;
+	
+	private String name;
 	private int price;
-	*/
+	
+	// Getter / Setter
+	// 은닉된 필드를 우회 접근하기 위한 메서드
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	// 메서드
+	public void showInfo() {
+		// 내부 필드의 정보를 출력
+		System.out.printf("%s, 가격 %d원%n", name, price);
+	}
 }
---
+++ JavaEx/src/com/java/oop/point/v1/PointApp.java
@@ -0,0 +1,19 @@
+package com.java.oop.point.v1;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		Point p1 = new Point();
+		p1.setX(10);
+		p1.setY(20);
+		
+		Point p2 = new Point();
+		p2.setX(20);
+		p2.setY(30);
+		
+		p1.draw();
+		p2.draw();
+
+	}
+
+}
